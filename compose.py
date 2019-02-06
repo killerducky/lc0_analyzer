877ba25f5e91601c4effb4bc0c01588138c42bc7
Andy Olsen <andy.olsen.tx@gmail.com>
2019-02-06 15:26:00 -0600

    Make plots 2 columns wide.

--- compose.py
+++ compose.py
@@ -3,6 +3,7 @@
 import svgutils.transform as sg
 from svgutils.compose import *
 import sys
+import math
 
 outplot = sys.argv[1]
 inplots = sys.argv[2:]
@@ -14,12 +15,14 @@ scale = 1.0
 
 fig = None
 for i, plot in enumerate(inplots):
+    x = i % 2
+    y = i // 2
     tmpfig = sg.fromfile(plot)
     xsize = float(tmpfig.width[:-2])   # chop "pt" off
     ysize = float(tmpfig.height[:-2])
-    if not fig: fig = sg.SVGFigure(xsize*scale, ysize*len(inplots)*scale)
+    if not fig: fig = sg.SVGFigure(xsize*2*scale, math.ceil(len(inplots)/2)*ysize*scale)
     tmpplot = tmpfig.getroot()
-    tmpplot.moveto(0, ysize*i)
+    tmpplot.moveto(x*xsize, y*ysize)
     fig.append(tmpplot)
 
 fig.save(outplot)
