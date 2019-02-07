2e54e4e4b3f00c633fc9817f0a23ab085f631005
Andy Olsen <andy.olsen.tx@gmail.com>
2019-02-07 13:23:35 -0600

    Get rid of old df array.
    Also add some sqlite3 code for fun.

--- tcec_parse.py
+++ tcec_parse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# wget https://tcec.chessdom.com/json/event.pgn
+
 import argparse
 import chess
 import chess.pgn
@@ -15,24 +17,16 @@ import matplotlib.pyplot as plt
 import matplotlib.axes
 import pandas as pd
 import numpy as np
-
-df2 = pd.DataFrame()
-#row = {"k":"1", "a":0}
-#df2 = df2.append(row, ignore_index=True)
-#row = {"k":"2", "a":3}
-#df2 = df2.append(row, ignore_index=True)
-#print(df2)
+import sqlite3
 
 with open("tcec14_sufi.pgn") as pgn:
     while True:
         game = chess.pgn.read_game(pgn)
         if not game:
             break
-        df = []
         df2 = pd.DataFrame()
         print(game.headers["Round"], game.headers["White"], game.headers["Black"], game.headers["Result"])
         for node in game.mainline():
-            df.append({})
             board = node.board()
             row = {}
             row["Round"] = game.headers["Round"]
@@ -44,35 +38,43 @@ with open("tcec14_sufi.pgn") as pgn:
                 row["movenum"] = board.fullmove_number
                 row["whomoved"] = "w"
             if node.comment.startswith("book"):
-                df[-1]["wv"] = "0.0"
+                row["wv"] = "0.0"
             else:
                 for item in node.comment.split(","):
                     if item == "": continue
                     k, _, v = item.partition("=")
                     k = re.sub(" ", "", k)
-                    df[-1][k] = v
                     row[k] = v
-            if df[-1]["wv"].startswith("-M"): df[-1]["wv"] = -999
-            elif df[-1]["wv"].startswith("M"): df[-1]["wv"] = 999
-            df[-1]["wv"] = float(df[-1]["wv"])
-            row["wv"] = df[-1]["wv"]
-            if not board.turn:
-                #print("%3s. %5s %5.2f" % (len(df)//2+1, node.san(), df[-1]["wv"]), end="")
-                print("%3s. %5s %5.2f" % (board.fullmove_number, node.san(), df[-1]["wv"]), end="")
-            else:
-                print("  %5s %5.2f" % (node.san(), df[-1]["wv"]), end="")
-                if len(df) > 2:
-                    if abs(df[-1]["wv"] - df[-3]["wv"]) > 0.5: print(" black moob/boom", end="")
-                    if abs(df[-2]["wv"] - df[-4]["wv"]) > 0.5: print(" white moob/boom", end="")
-                print()
+            if row["wv"].startswith("-M"): row["wv"] = -999
+            elif row["wv"].startswith("M"): row["wv"] = 999
+            row["wv"] = float(row["wv"])
             df2 = df2.append(row, ignore_index=True)
+            if not board.turn:
+                print("%3s. %5s %5.2f" % (board.fullmove_number, node.san(), row["wv"]), end="")
+            else:
+                print("  %5s %5.2f" % (node.san(), row["wv"]), end="")
+                if row["movenum"] > 1:
+                    if abs(df2.iloc[-1]["wv"] - df2.iloc[-3]["wv"]) > 0.5: print(" black moob/boom", end="")
+                    if abs(df2.iloc[-2]["wv"] - df2.iloc[-4]["wv"]) > 0.5: print(" white moob/boom", end="")
+                print()
         print()
         print(df2)
         fig, ax = plt.subplots()
         for color in ("w", "b"):
             tmp = df2[df2["whomoved"] == color]
             tmp.plot.line(x="movenum", y="wv", ax=ax)
-        ax.legend("w", "b")
-        ax.set_ylim(-10, 10)
+        ax.legend(["w", "b"])
+        ax.set_ylim(-5, 5)
         plt.savefig("tcec_14_sufi_round_%s.svg" % (game.headers["Round"].rjust(4, "0")))
         plt.close("all")
+        #conn = sqlite3.connect("games.db")
+        #df2.to_sql("games", conn)
+        #break
+
+#conn = sqlite3.connect("games.db")
+##cur = conn.cursor()
+##df2 = cur.execute("select * from games").fetchall()
+#df2 = pd.read_sql_query("select * from games;", conn)
+#print(df2)
+#raise
+
