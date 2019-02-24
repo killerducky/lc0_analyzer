5f96960501de5bf42a10c121ccfebeafb6191ac9
Andy Olsen <andy.olsen.tx@gmail.com>
2019-02-23 22:48:32 -0600

    tcec_parse: Add a few things

--- tcec_parse.py
+++ tcec_parse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# wget https://tcec.chessdom.com/json/event.pgn
+# wget -O tcec14_sufi.pgn https://tcec.chessdom.com/json/event.pgn
 
 import argparse
 import chess
@@ -19,6 +19,8 @@ import pandas as pd
 import numpy as np
 import sqlite3
 
+float_keys = ["wv", "s", "mt", "n"]
+
 with open("tcec14_sufi.pgn") as pgn:
     while True:
         game = chess.pgn.read_game(pgn)
@@ -31,6 +33,8 @@ with open("tcec14_sufi.pgn") as pgn:
             row = {}
             row["Round"] = game.headers["Round"]
             row["sanmove"] = node.san()
+            row["White"] = game.headers["White"]
+            row["Black"] = game.headers["Black"]
             if board.turn:
                 row["movenum"] = board.fullmove_number-1
                 row["whomoved"] = "b"
@@ -47,7 +51,9 @@ with open("tcec14_sufi.pgn") as pgn:
                     row[k] = v
             if row["wv"].startswith("-M"): row["wv"] = -999
             elif row["wv"].startswith("M"): row["wv"] = 999
-            row["wv"] = float(row["wv"])
+            for key in float_keys:
+                if not key in row: row[key] = 0
+                row[key] = float(row[key])
             df2 = df2.append(row, ignore_index=True)
             if not board.turn:
                 print("%3s. %5s %5.2f" % (board.fullmove_number, node.san(), row["wv"]), end="")
@@ -58,14 +64,27 @@ with open("tcec14_sufi.pgn") as pgn:
                     if abs(df2.iloc[-2]["wv"] - df2.iloc[-4]["wv"]) > 0.5: print(" white moob/boom", end="")
                 print()
         print()
-        print(df2)
         fig, ax = plt.subplots()
         for color in ("w", "b"):
             tmp = df2[df2["whomoved"] == color]
             tmp.plot.line(x="movenum", y="wv", ax=ax)
         ax.legend(["w", "b"])
         ax.set_ylim(-5, 5)
-        plt.savefig("tcec_14_sufi_round_%s.svg" % (game.headers["Round"].rjust(4, "0")))
+        plt.savefig("tcec_14_sufi_round_%s_%s.svg" % (game.headers["Round"].rjust(4, "0"), "wv"))
+        plt.close("all")
+
+        fig, ax = plt.subplots()
+        ax2 = ax.twinx()
+        color = "w" if df2.iloc[1]["White"].startswith("LCZero") else "b"
+        tmp = df2[df2["whomoved"] == color]
+        print("max lc0 time %f" % (tmp["mt"].max()/1000))
+        tmp.plot.line(x="movenum", y="s", ax=ax)
+        #tmp.plot.line(x="movenum", y="mt", ax=ax)
+        tmp.plot.line(x="movenum", y="n", color="red", ax=ax2)
+        #ax.legend(["w", "b"])
+        ax.set_ylim(0, 300000)
+        ax2.set_ylim(0, 20000000)
+        plt.savefig("tcec_14_sufi_round_%s_%s.svg" % (game.headers["Round"].rjust(4, "0"), "s"))
         plt.close("all")
         #conn = sqlite3.connect("games.db")
         #df2.to_sql("games", conn)
